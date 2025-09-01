c81eb4c0cb92c942385c67481fd44f14df67b8f3
wa1ead <waliddraa2@gmail.com>
2025-09-01 17:15:30 +0100

    modify the world span

--- src/components/PopularMoviesList.jsx
+++ src/components/PopularMoviesList.jsx
@@ -5,8 +5,9 @@ function PopularMoviesList({ movies }) {
     <div className="mx-8 pb-8 rounded-xl bg-gradient-to-br from-blue-100 via-white to-blue-300 shadow-xl">
       <h2 className="py-8 text-4xl font-extrabold text-blue-900 text-center tracking-wide drop-shadow-lg">
         <span className="inline-block bg-gradient-to-r from-blue-700 via-blue-400 to-blue-700 bg-clip-text text-transparent">
-          Trending Movies 🌍
+          Trending Movies
         </span>
+        <span>🌍</span>
       </h2>
       <div className="grid grid-cols-3 gap-10 max-md:grid-cols-1 max-lg:grid-cols-2">
         {movies.map((movie) => (
